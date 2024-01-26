82c3c281603a2e0a8a88d0e42a8f8369bb8a883b
David Bravo Cisterna <bravocisterna@gmail.com>
2024-01-25 21:53:36 -0300

    agrego link en vez de button en experiences

--- Frontend/src/components/Experiencias.jsx
+++ Frontend/src/components/Experiencias.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom'; // Importar Link
 
 const Experiencias = ({ experienceCard }) => {
-    const navigate = useNavigate();
 
     experienceCard.forEach(experience => {
         experience.tags = [
@@ -23,18 +22,20 @@ const Experiencias = ({ experienceCard }) => {
                                 <div>{experience.experience_video}</div>
                                 <p className="text-gray-900 pt-4 font-bold text-l">{experience.experience_name}</p>
                                 <div className="mt-1">
+                                    {/* Etiquetas de la experiencia */}
                                     <div className="inline-flex items-center rounded-md bg-gray-500 mr-2 mb-1 px-2 py-1 text-xs font-medium text-gray-50 ring-1 ring-inset ring-gray-500/10">{experience.experience_type}</div>
                                     <div className="inline-flex items-center rounded-md bg-gray-500 mr-2 mb-1 px-2 py-1 text-xs font-medium text-gray-50 ring-1 ring-inset ring-gray-500/10">{experience.experience_country}</div>
                                     <div className="inline-flex items-center rounded-md bg-gray-500 mr-2 mb-1 px-2 py-1 text-xs font-medium text-gray-50 ring-1 ring-inset ring-gray-500/10">{experience.experience_main_discipline}</div>
                                     <div className="inline-flex items-center rounded-md bg-gray-500 mr-2 mb-1 px-2 py-1 text-xs font-medium text-gray-50 ring-1 ring-inset ring-gray-500/10">{experience.experience_demand_level}</div>
                                 </div>
-                                <button 
-                                    onClick={() => navigate(`/tarjeta/${experience.experience_id}`)}
+                                {/* Link a la tarjeta de la experiencia */}
+                                <Link 
+                                    to={`/tarjeta/${experience.experience_id}`}
                                     className="mt-1 text-gray-700 bg-letrip focus:outline-none focus:text-black focus:bg-gray-50 focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                                     style={{ position: 'relative', zIndex: 2 }}
                                 >
                                     <strong>{experience.experience_price}<span> USD por persona</span></strong>
-                                </button>              
+                                </Link>
                             </div>
                         </div>
                     ))}
