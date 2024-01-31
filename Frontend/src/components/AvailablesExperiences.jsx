f2452e6d85e0637fcf1bd9422259c9e8e755b506
David Bravo Cisterna <bravocisterna@gmail.com>
2024-01-31 02:35:39 -0300

    correcciones vercel

--- Frontend/src/components/AvailablesExperiences.jsx
+++ Frontend/src/components/AvailablesExperiences.jsx
@@ -7,7 +7,7 @@ const AvailablesExperiences = () => {
     const [selectedExperience, setSelectedExperience] = useState(null);
 
     useEffect(() => {
-        fetch('http://letrip13012023-backend-rxcbsi08r-lawitec.vercel.app/experiences')
+        fetch('https://https://letrip13012023-backend-lawitec.vercel.app/')
             .then(response => response.json())
             .then(data => setExperiences(data))
             .catch(error => console.error('Error:', error));
