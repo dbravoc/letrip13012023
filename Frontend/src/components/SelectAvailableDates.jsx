1631dfbff57578302abc901ac574433f5e9ac888
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 01:10:21 -0300

    Update SelectAvailableDates.jsx

--- Frontend/src/components/SelectAvailableDates.jsx
+++ Frontend/src/components/SelectAvailableDates.jsx
@@ -2,19 +2,17 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 const SelectAvailableDates = ({ experienceCard }) => {
-  const { id } = useParams(); // Obtiene el ID de la URL
-  const selectedExperience = experienceCard.find(e => e.experience_uuid.toString() === id);
+  const { id } = useParams();
+  // Asegúrate de que experienceCard no es undefined
+  const selectedExperience = experienceCard?.find(e => e.experience_uuid.toString() === id);
 
-  // Verificar si la experiencia seleccionada está cargada
-  if (!selectedExperience) {
-    return <div>Cargando experiencia...</div>; // Mostrar un mensaje mientras se carga
+  // Comprobación para esperar la carga de experienceCard o si no se encuentra la experiencia
+  if (!experienceCard || experienceCard.length === 0 || !selectedExperience) {
+    return <div>Cargando experiencia...</div>; // o "Experiencia no encontrada" si experienceCard ya está cargado pero no selectedExperience
   }
 
-  // Asumiendo que 'available_dates' es un array en tu objeto de experiencia
-  // y ya está en el formato adecuado (no necesita JSON.parse)
   const { available_dates } = selectedExperience;
 
-  // Comprobación adicional para asegurar que hay fechas disponibles
   if (!available_dates || available_dates.length === 0) {
     return <div>No hay fechas disponibles para esta experiencia.</div>;
   }
@@ -24,7 +22,6 @@ const SelectAvailableDates = ({ experienceCard }) => {
       <h3 className="mb-10 text-2xl font-bold tracking-tight text-gray-900">Selecciona una fecha disponible</h3>
       <ul>
         {available_dates.map((date, index) => (
-          // Asegúrate de que 'startDate' y 'endDate' son propiedades válidas de tus objetos de fecha
           <li key={index}>{`${date.startDate} al ${date.endDate}`}</li>
         ))}
       </ul>
