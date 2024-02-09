59637363245bb687fa3325adaf487b432d086db0
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 11:55:39 -0300

    cuantos

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -63,9 +63,9 @@ const App = () => {
             <div className='grid grid-cols-1 sm:grid-cols-2'>
               <div className='justify-self-center'>
                   <div className='pt-36'><Tarjeta experienceCard={experienceCard} /></div>
-                  <div className='pt-20'><IncludedNotIncluded experienceCard={experienceCard} /></div>
-                  <div className='pt-36'><SelectAvailableDates experienceCard={experienceCard}/></div>
-                  <div className='pt-36'><SelectPlayers experienceCard={experienceCard}/></div>
+                  <div className='pt-15'><IncludedNotIncluded experienceCard={experienceCard} /></div>
+                  <div className='pt-15'><SelectAvailableDates experienceCard={experienceCard}/></div>
+                  <div className='pt-15'><SelectPlayers experienceCard={experienceCard}/></div>
 
               </div>
               <div className='justify-self-center'>
--- Frontend/src/components/SelectPlayers.jsx
+++ Frontend/src/components/SelectPlayers.jsx
@@ -11,26 +11,28 @@ const SelectPlayers = ({ experienceCard }) => {
 
   return (
     <div className="mx-0 sm:px-6 sm:py-8 mb-10 tracking-tight text-gray-900">
-      <h3 className="text-2xl font-bold">Selecciona la cantidad de personas </h3>
-      <p>Mínimo permitido: {experienceCard.minimum_group_size} personas</p>
-      <p>Máximo permitido: {experienceCard.max_group_size} personas</p>
+      <h3 className="text-2xl font-bold">¿Cuántas personas son? </h3>
 
       {experienceCard && (
         <div className="mb-4">
           <input
             type="number"
+            placeholder='¿Cuántos?'
             id="players"
             name="players"
             min={experienceCard.minimum_group_size}
             max={experienceCard.max_group_size}
             onChange={handlePlayerChange}
-            className="border border-gray-400 rounded-md px-4 py-2"
+            className="outline-none border border-gray-400 rounded-md px-4 py-2"
           />
-          {selectedPlayers && (
-            <p>Has seleccionado {selectedPlayers} personas.</p>
-          )}
+          {selectedPlayers}
         </div>
       )}
+
+    <div className='text-xs italic text-gray-500'>
+        <p>Mínimo permitido: {experienceCard.minimum_group_size} personas</p>
+        <p>Máximo permitido: {experienceCard.max_group_size} personas</p>
+    </div>
     </div>
   );
 };
