6bca34b706e49d9a9351cb6289bac2a59919a109
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 15:09:00 -0300

    Update PriceExperience.jsx

--- Frontend/src/components/PriceExperience.jsx
+++ Frontend/src/components/PriceExperience.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
 
-const PriceExperience = ({ ExperienceCard }) => {
-  // Verificar si ExperienceCard existe y tiene la propiedad experience_price
-  const experiencePrice = ExperienceCard?.experience_price;
+const PriceExperience = ({ experienceCard }) => {
+  // Asumiendo que quieres mostrar el precio de la primera experiencia
+  // O asegúrate de seleccionar la experiencia correcta de alguna manera
+  const experiencePrice = experienceCard.experiences?.[0]?.experience_price;
 
   return (
     <div className="price-container">
       <h3 className="text-2xl font-bold mb-4">Precio de la Experiencia</h3>
       {experiencePrice ? (
-        // Si experiencePrice existe, mostrar el precio
         <p className="text-lg">
           $US<span className="font-semibold">{experiencePrice}</span> por persona
         </p>
       ) : (
-        // Si experiencePrice no existe, mostrar un mensaje predeterminado
         <p className="text-lg">Precio no disponible</p>
       )}
     </div>
