5e7311a047a072ffdde30782b4455616b533b084
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 09:56:15 -0300

    Update SelectAvailableDates.jsx

--- Frontend/src/components/SelectAvailableDates.jsx
+++ Frontend/src/components/SelectAvailableDates.jsx
@@ -21,7 +21,7 @@ const SelectAvailableDates = ({ experienceCard }) => {
           {dates.map((date, index) => (
             <button
               key={index}
-              className={`px-4 py-2 rounded-md text-letrip font-medium ${selectedDate === `${date.startDate}-${date.endDate}` ? 'bg-black' : 'bg-gray-500 hover:bg-gray-700'}`}
+              className={`px-4 py-2 rounded-md font-medium ${selectedDate === `${date.startDate}-${date.endDate}` ? 'bg-black text-yellow-500' : 'bg-yellow-500 hover:bg-yellow-700 text-black'}`}
               onClick={() => setSelectedDate(`${date.startDate}-${date.endDate}`)}
             >
               Desde: {date.startDate}, Hasta: {date.endDate}
