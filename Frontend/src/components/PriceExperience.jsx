08295dc0747995b409202086e06937434b2dcc42
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 15:26:31 -0300

    Update PriceExperience.jsx

--- Frontend/src/components/PriceExperience.jsx
+++ Frontend/src/components/PriceExperience.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBills } from '@fortawesome/free-solid-svg-icons';
+
 
 const PriceExperience = ({ experienceCard }) => {
   const { id } = useParams(); // Obtiene el ID de la URL
   const selectedExperience = experienceCard.find(e => e.experience_uuid === id);
 
   return (
-    <div className="flex px-0 mx-0">
-      <h3 className="text-2xl font-bold mb-4">Precio de la Experiencia</h3>
+    <div className="mx-0 sm:px-6 sm:py-8 mb-10 tracking-tight text-gray-900">
+      <h3 className="text-2xl font-bold mb-10">Precio de la Experiencia</h3>
       {selectedExperience ? (
-        <div className=" pt-2">
-          <p>US${selectedExperience.experience_price} por persona</p>
+        <div className="mb-4">
+          <p className='text-sm'> <FontAwesomeIcon icon={faMoneyBills} /> US$ <span className='text-xl font-semibold'> {selectedExperience.experience_price} </span>por persona</p>
         </div>
       ) : (
         <div>Precio no disponible</div>
