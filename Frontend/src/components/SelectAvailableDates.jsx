0362f98469754df8edcf60f1977ada61bf3d1a35
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 01:18:01 -0300

    update

--- Frontend/src/components/SelectAvailableDates.jsx
+++ Frontend/src/components/SelectAvailableDates.jsx
@@ -1,30 +1,25 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { format } from 'date-fns';
 
 const SelectAvailableDates = ({ experienceCard }) => {
-  const { id } = useParams();
-  // Asegúrate de que experienceCard no es undefined
-  const selectedExperience = experienceCard?.find(e => e.experience_uuid.toString() === id);
-
-  // Comprobación para esperar la carga de experienceCard o si no se encuentra la experiencia
-  if (!experienceCard || experienceCard.length === 0 || !selectedExperience) {
-    return <div>Cargando experiencia...</div>; // o "Experiencia no encontrada" si experienceCard ya está cargado pero no selectedExperience
-  }
-
-  const { available_dates } = selectedExperience;
-
-  if (!available_dates || available_dates.length === 0) {
-    return <div>No hay fechas disponibles para esta experiencia.</div>;
-  }
+  // Asumiendo que experienceCard es un objeto que contiene un array de available_dates
+  // Y que cada elemento en available_dates es un objeto con startDate y endDate
+  
+  const handleChange = (e) => {
+    console.log("Fecha seleccionada:", e.target.value);
+    // Aquí puedes manejar la fecha seleccionada, por ejemplo, actualizar el estado o enviarla a otro componente
+  };
 
   return (
     <div>
-      <h3 className="mb-10 text-2xl font-bold tracking-tight text-gray-900">Selecciona una fecha disponible</h3>
-      <ul>
-        {available_dates.map((date, index) => (
-          <li key={index}>{`${date.startDate} al ${date.endDate}`}</li>
+      <label htmlFor="availableDates">Seleccione una fecha disponible:</label>
+      <select id="availableDates" onChange={handleChange}>
+        {experienceCard.available_dates?.map((dateRange, index) => (
+          <option key={index} value={format(new Date(dateRange.startDate), 'dd-MM-yyyy')}>
+            {format(new Date(dateRange.startDate), 'dd-MM-yyyy')} - {format(new Date(dateRange.endDate), 'dd-MM-yyyy')}
+          </option>
         ))}
-      </ul>
+      </select>
     </div>
   );
 };
