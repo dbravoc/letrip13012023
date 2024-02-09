a3fd2eef0d3e371d71c753b019dc5c538a8fe4f9
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-09 00:58:20 -0300

    Update SelectAvailableDates.jsx

--- Frontend/src/components/SelectAvailableDates.jsx
+++ Frontend/src/components/SelectAvailableDates.jsx
@@ -1,50 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 
-const SelectAvailableDates = () => {
-  const { id: uuid } = useParams();
-  const [experience, setExperience] = useState(null);
-  const [loading, setLoading] = useState(true);
+const SelectAvailableDates = ({ experienceCard }) => {
+  const { id } = useParams(); // Obtiene el ID de la URL
+  const selectedExperience = experienceCard.find(e => e.experience_uuid.toString() === id);
 
-  useEffect(() => {
-    const fetchExperienceData = async () => {
-      try {
-        // Suponiendo que tu API soporte obtener una experiencia específica por su ID/UUID
-        const response = await fetch(`https://letrip13012023-backend-lawitec.vercel.app/experiences/${uuid}`);
-        const data = await response.json();
-        
-        // Verificar si la experiencia tiene fechas disponibles
-        if (data && data.available_dates) {
-          setExperience(data);
-        } else {
-          setExperience(null); // Asegúrate de manejar la ausencia de datos adecuadamente
-        }
-      } catch (error) {
-        console.error('Error al obtener la experiencia:', error);
-        setExperience(null);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchExperienceData();
-  }, [uuid]);
-
-  if (loading) {
-    return <p>Cargando fechas disponibles...</p>;
+  // Verificar si la experiencia seleccionada está cargada
+  if (!selectedExperience) {
+    return <div>Cargando experiencia...</div>; // Mostrar un mensaje mientras se carga
   }
 
-  if (!experience || !experience.available_dates) {
-    return <p>No hay fechas disponibles.</p>;
+  // Asumiendo que 'available_dates' es un array en tu objeto de experiencia
+  // y ya está en el formato adecuado (no necesita JSON.parse)
+  const { available_dates } = selectedExperience;
+
+  // Comprobación adicional para asegurar que hay fechas disponibles
+  if (!available_dates || available_dates.length === 0) {
+    return <div>No hay fechas disponibles para esta experiencia.</div>;
   }
 
-  // Asumiendo que 'available_dates' es un array y no necesita ser parseado
-  const dates = experience.available_dates;
   return (
     <div>
       <h3 className="mb-10 text-2xl font-bold tracking-tight text-gray-900">Selecciona una fecha disponible</h3>
       <ul>
-        {dates.map((date, index) => (
+        {available_dates.map((date, index) => (
+          // Asegúrate de que 'startDate' y 'endDate' son propiedades válidas de tus objetos de fecha
           <li key={index}>{`${date.startDate} al ${date.endDate}`}</li>
         ))}
       </ul>
