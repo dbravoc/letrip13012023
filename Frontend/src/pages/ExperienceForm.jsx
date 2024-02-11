44d2a95dac41b20dfa7a9ad99ddbb3ee11342b87
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-11 17:07:08 -0300

    Update ExperienceForm.jsx

--- Frontend/src/pages/ExperienceForm.jsx
+++ Frontend/src/pages/ExperienceForm.jsx
@@ -461,34 +461,38 @@ const method = mode === 'create' ? 'POST' : 'PUT';
          onChange={handleImageUpload} // Manejar la carga de imágenes en una función
          className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
        />
-<h3 className="my-10 text-2xl font-bold tracking-tight text-gray-900">Fechas disponibles</h3>
-<>
-  <DateRangePicker
-  onChange={item => {
-    const newRange = {
-      startDate: item.selection.startDate,
-      endDate: item.selection.endDate,
-      key: 'selection',
-    };
-    setCurrentRange([newRange]); // Asegurarse de actualizar el estado con el nuevo rango
-  }}
-  showSelectionPreview={true}
-  moveRangeOnFirstSelection={false}
-  months={2}
-  ranges={currentRange}
-  direction="horizontal"
-  />
-  
-  <button className='block rounded-md bg-gray-900 px-1 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700' type="button" onClick={addRange}>Agregar disponibilidad</button>
-  <p className="mt-10 text-lg font-bold tracking-tight text-gray-900">Fechas seleccionadas</p>
 
-  {dateRanges.map((range, index) => (
-    <div className='flex justify-between border-b-2' key={index}>
-      <p className='text-sm flex items-center'> <span className='font-semibold text-black  py-0'> {range.startDate}</span> <span className='px-2'>al</span> <span className='font-semibold text-black'>{range.endDate}</span></p>
-      <button className='block rounded-md px-1 py-2 text-center text-sm font-semibold text-red-400 hover:text-red-500 shadow-sm' type="button" onClick={() => removeRange(index)}>Eliminar</button>
-    </div>
-  ))}
-</>
+
+{mode !== 'update' && (
+        <>
+          <h3 className="my-10 text-2xl font-bold tracking-tight text-gray-900">Fechas disponibles</h3>
+          <DateRangePicker
+            onChange={item => {
+              const newRange = {
+                startDate: item.selection.startDate,
+                endDate: item.selection.endDate,
+                key: 'selection',
+              };
+              setCurrentRange([newRange]); // Asegurarse de actualizar el estado con el nuevo rango
+            }}
+            showSelectionPreview={true}
+            moveRangeOnFirstSelection={false}
+            months={2}
+            ranges={currentRange}
+            direction="horizontal"
+          />
+          
+          <button className='block rounded-md bg-gray-900 px-1 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-gray-700' type="button" onClick={addRange}>Agregar disponibilidad</button>
+          <p className="mt-10 text-lg font-bold tracking-tight text-gray-900">Fechas seleccionadas</p>
+
+          {dateRanges.map((range, index) => (
+            <div className='flex justify-between border-b-2' key={index}>
+              <p className='text-sm flex items-center'> <span className='font-semibold text-black  py-0'> {range.startDate}</span> <span className='px-2'>al</span> <span className='font-semibold text-black'>{range.endDate}</span></p>
+              <button className='block rounded-md px-1 py-2 text-center text-sm font-semibold text-red-400 hover:text-red-500 shadow-sm' type="button" onClick={() => removeRange(index)}>Eliminar</button>
+            </div>
+          ))}
+        </>
+      )}
 <h3 className="my-10 text-2xl font-bold tracking-tight text-gray-900">Restricciones</h3>
       <label className='text-gray-700 text-sm' htmlFor="target_audience_restrictions">Restricciones de la experiencia<span className='text-xs italic'> (Menciona las restricciones que pueden haber en una experiencia, tales como estado de salud, condición física, entre otros. Escribe algo breve y específico) </span></label>
       <input
