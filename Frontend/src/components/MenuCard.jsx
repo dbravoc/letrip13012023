961b41afdf65557bbdecf79df7dd50c7668a79ff
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-21 15:08:50 -0300

    Update MenuCard.jsx

--- Frontend/src/components/MenuCard.jsx
+++ Frontend/src/components/MenuCard.jsx
@@ -8,16 +8,17 @@ const MenuCard = () => {
     return (
 <div>
     <div className="mx-auto flex justify-center">
+    <h3 className="mb-10 text-2xl font-bold tracking-tight text-gray-900">Panel de administración</h3>
         <div className="grid grid-cols-1 sm:gap-6 gap-0">
             {/* Opción para Crear nueva experiencia */}
             <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faSquarePlus} />
+                <div className="flex w-auto justify-between leading-normal">
                     <Link 
                         to="/create"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
-                    >
+                    > 
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faSquarePlus} />
                         <strong>Crear una nueva experiencia</strong>
                     </Link>
                 </div>
@@ -25,36 +26,39 @@ const MenuCard = () => {
             {/* Opción para Editar información general */}
             <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
                 <div className="flex max-w-[350px] justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faPenToSquare} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
                     >
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faPenToSquare} />
+
                         <strong>Editar información de una experiencia</strong>
                     </Link>
                 </div>
             </div>
             <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
                 <div className="flex max-w-[350px] justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faImage} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
                     >
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faImage} />
+
                         <strong>Editar imágenes de una experiencia</strong>
                     </Link>
                 </div>
             </div>
             <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
                 <div className="flex max-w-[350px] justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faCalendarCheck} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
                     >
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faCalendarCheck} />
+
                         <strong>Editar disponibilidad de una experiencia</strong>
                     </Link>
                 </div>
