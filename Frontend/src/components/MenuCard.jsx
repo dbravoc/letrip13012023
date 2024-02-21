f330c0c529787156f19c6a3b27382ae390c69533
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-21 16:07:44 -0300

    nuevo panel admin

--- Frontend/src/components/MenuCard.jsx
+++ Frontend/src/components/MenuCard.jsx
@@ -6,67 +6,54 @@ import { Link } from 'react-router-dom';
 
 const MenuCard = () => {
     return (
-<div>
     <div className="mx-auto flex flex-col justify-center">
     <h3 className="flex justify-center mb-10 text-2xl font-bold tracking-tight text-gray-900">Panel de administración</h3>
-        <div className="w-3/4 grid grid-cols-1 justify-center gap-10">
-            <div className="flex justify-start items-center rounded-lg overflow-hidden px-0 py-1">
+        <div className="grid grid-cols-1 sm:grid-cols-2 justify-center gap-10">
+            <div className="flex max-w-md justify-start rounded-lg overflow-hidden  p-4">
                     <Link 
                         to="/create"
-                        className="w-full hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
+                        className="w-full flex items-center  hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
                     > 
-                    <FontAwesomeIcon className="text-gray-900 mx-1 font-bold text-5xl" icon={faSquarePlus} />
+                    <FontAwesomeIcon className="text-gray-900 p-2 mx-1 font-bold text-6xl" icon={faSquarePlus} />
                         <strong>Crear una nueva experiencia</strong>
                     </Link>
             </div>
 
-            {/* Opción para Editar información general */}
-            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] justify-between leading-normal">
+            <div className="flex justify-start rounded-lg overflow-hidden p-4">
                     <Link 
-                        to="/update-info"
-                        className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
+                        to="/create"
+                        className="w-full flex items-center  hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
-                    >
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faPenToSquare} />
-
+                    > 
+                    <FontAwesomeIcon className="text-gray-900 mx-1 p-2 font-bold text-6xl" icon={faPenToSquare} />
                         <strong>Editar información de una experiencia</strong>
                     </Link>
-                </div>
             </div>
-            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] justify-between leading-normal">
-                    <Link 
-                        to="/update-info"
-                        className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
-                        style={{ position: 'relative', zIndex: 2 }}
-                    >
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faImage} />
 
+            <div className="flex justify-start rounded-lg overflow-hidden  p-4">
+                    <Link 
+                        to="/create"
+                        className="w-full flex items-center  hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
+                        style={{ position: 'relative', zIndex: 2 }}
+                    > 
+                    <FontAwesomeIcon className="text-gray-900 mx-1 p-2 font-bold text-6xl" icon={faImage} />
                         <strong>Editar imágenes de una experiencia</strong>
                     </Link>
-                </div>
             </div>
-            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] justify-between leading-normal">
-                    <Link 
-                        to="/update-info"
-                        className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
-                        style={{ position: 'relative', zIndex: 2 }}
-                    >
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-5xl" icon={faCalendarCheck} />
 
+            <div className="flex justify-start rounded-lg overflow-hidden  p-4">
+                    <Link 
+                        to="/create"
+                        className="w-full flex items-center  hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
+                        style={{ position: 'relative', zIndex: 2 }}
+                    > 
+                    <FontAwesomeIcon className="text-gray-900 mx-1 p-2 font-bold text-6xl" icon={faCalendarCheck} />
                         <strong>Editar disponibilidad de una experiencia</strong>
                     </Link>
-                </div>
             </div>
-            
-            {/* Más opciones siguiendo el mismo patrón */}
-            {/* ... */}
         </div>
     </div>
-</div>
   );
 };
 
