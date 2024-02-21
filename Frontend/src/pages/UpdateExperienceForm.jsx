1474b40577ce359ed9cca1428a4a763fcb02a263
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-21 13:18:10 -0300

    Update UpdateExperienceForm.jsx

--- Frontend/src/pages/UpdateExperienceForm.jsx
+++ Frontend/src/pages/UpdateExperienceForm.jsx
@@ -58,9 +58,9 @@ const UpdateExperienceForm = () => {
 
   return (
     <div>
-      <h2>Actualizar Experiencia</h2>
+      <h3 className="mb-10 text-2xl font-bold tracking-tight text-gray-900">Actualizar Experiencia</h3>
       <select onChange={handleExperienceChange} value={selectedExperience?.experience_uuid || ''}
-        className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
+        className="flex flex-col px-auto sm:px-72 gap-y-2 text-sm w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
         >
         <option value="">Seleccione una experiencia</option>
         {experiences.map((experience) => (
