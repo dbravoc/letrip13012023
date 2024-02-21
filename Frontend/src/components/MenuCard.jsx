be3da41028ce4071edb6fcafd9faf03b1d0958da
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-21 15:00:01 -0300

    Update MenuCard.jsx

--- Frontend/src/components/MenuCard.jsx
+++ Frontend/src/components/MenuCard.jsx
@@ -8,24 +8,24 @@ const MenuCard = () => {
     return (
 <div>
     <div className="mx-auto flex justify-center">
-        <div className="grid sm:grid-cols-3 grid-cols-1 sm:gap-6 gap-0">
+        <div className="grid grid-cols-1 sm:gap-6 gap-0">
             {/* Opción para Crear nueva experiencia */}
-            <div className="flex flex-col justify-center mb-10 md:flex-row rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] flex-col justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-l" icon={faSquarePlus} />
+            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
+                <div className="flex max-w-[350px] justify-between leading-normal">
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faSquarePlus} />
                     <Link 
                         to="/create"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
                         style={{ position: 'relative', zIndex: 2 }}
                     >
-                        <strong>Crear una   nueva experiencia</strong>
+                        <strong>Crear una nueva experiencia</strong>
                     </Link>
                 </div>
             </div>
             {/* Opción para Editar información general */}
-            <div className="flex flex-col justify-center mb-10 md:flex-row rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] flex-col justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-l" icon={faPenToSquare} />
+            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
+                <div className="flex max-w-[350px] justify-between leading-normal">
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faPenToSquare} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
@@ -35,9 +35,9 @@ const MenuCard = () => {
                     </Link>
                 </div>
             </div>
-            <div className="flex flex-col justify-center mb-10 md:flex-row rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] flex-col justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-l" icon={faImage} />
+            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
+                <div className="flex max-w-[350px] justify-between leading-normal">
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faImage} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
@@ -47,9 +47,9 @@ const MenuCard = () => {
                     </Link>
                 </div>
             </div>
-            <div className="flex flex-col justify-center mb-10 md:flex-row rounded-lg overflow-hidden px-0">
-                <div className="flex max-w-[350px] flex-col justify-between leading-normal">
-                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-l" icon={faCalendarCheck} />
+            <div className="flex justify-center mb-10 rounded-lg overflow-hidden px-0">
+                <div className="flex max-w-[350px] justify-between leading-normal">
+                    <FontAwesomeIcon className="text-gray-900 pt-4 font-bold text-4xl" icon={faCalendarCheck} />
                     <Link 
                         to="/update-info"
                         className="mt-1 hover:bg-black hover:text-letrip bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-1 ease-in-out duration-50 text-center"
