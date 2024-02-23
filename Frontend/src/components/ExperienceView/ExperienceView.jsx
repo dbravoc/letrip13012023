71d37fd1fba8183910031af1413c54a6a38d8177
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-23 12:00:28 -0300

    Update ExperienceView.jsx

--- Frontend/src/components/ExperienceView/ExperienceView.jsx
+++ Frontend/src/components/ExperienceView/ExperienceView.jsx
@@ -21,8 +21,37 @@ const ExperienceView = ({ experienceCard }) => {
     { name: 'Ciudad de la experiencia', description: selectedExperience.experience_location },
   ];
 
-  return ( //GRID: seccion fotos, seccion informacion + invitacion anfitrion, reserva
-      
+  return (
+    <>
+  <div className="flex flex-col p-10 mx-auto bg-yellow-100 border-white border-2 rounded-2xl">
+  <div>
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 mb-4">{selectedExperience.experience_name}</h2>
+        
+            
+          <div className="flex justify-between py-5 items-center min-w-0 gap-x-4">
+            <div className="flex items-center min-w-0 gap-x-4">
+              <div className="h-24 w-24 flex-none rounded-full overflow-hidden bg-gray-50">
+                <img src={selectedExperience.instructor_profile_img} alt="" className='w-full h-full object-cover' />
+              </div>
+              
+              <div>
+                    <p className="text-base font-semibold leading-6 text-gray-900">{selectedExperience.experience_instructor}</p>
+                    <p className="text-sm leading-6 text-gray-900">{selectedExperience.experience_instructor_type}</p>
+              </div>
+
+            </div>
+
+            <div>
+                <p className=' text-xl font-semibold leading-6 text-gray-900'>{selectedExperience.experience_instructor_evaluation} <FontAwesomeIcon icon={faHeart} /></p>
+            </div>
+         </div>
+
+         <div>
+            <p className="text-sm py-5 leading-6 text-gray-900">{selectedExperience.experience_instructor_message}</p>
+        </div>
+
+  </div>
+
   <div className="flex flex-col p-10 mx-auto bg-yellow-100 border-white border-2 rounded-2xl">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 mb-4">Características de la experiencia</h2>
           <dl className="mt-0 grid grid-cols-1 sm:grid-cols-2 sm:gap-y-2 gap-x-6 gap-y-2">
@@ -33,8 +62,11 @@ const ExperienceView = ({ experienceCard }) => {
               </div>
             ))}
           </dl>
-      </div>  
+  </div>
+  </div>
+
   
+  </>
 
   );
   
