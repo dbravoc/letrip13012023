170faaccc24403519ac9885e10939999571007fd
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-23 12:50:42 -0300

    Update ExperienceView.jsx

--- Frontend/src/components/ExperienceView/ExperienceView.jsx
+++ Frontend/src/components/ExperienceView/ExperienceView.jsx
@@ -20,6 +20,20 @@ const ExperienceView = ({ experienceCard }) => {
     { name: 'País de la experiencia', description: selectedExperience.experience_country },
     { name: 'Ciudad de la experiencia', description: selectedExperience.experience_location },
   ];
+  const included = [
+    { name: 'Clase práctica', value: selectedExperience.included_practical_lessons },
+    { name: 'Clase teórica', value: selectedExperience.included_theoretical_lessons },
+    { name: 'Yoga', value: selectedExperience.included_yoga },
+    { name: 'Entrenamiento', value: selectedExperience.included_training },
+    { name: 'Video experiencia', value: selectedExperience.included_experience_video },
+    { name: 'Seguro accidentes', value: selectedExperience.included_accident_insurance },
+    { name: 'Arriendo equipos', value: selectedExperience.included_equipment_rental },
+    { name: 'Entrada a parques y eventos', value: selectedExperience.included_entry_fees },
+    { name: 'Ticket de Andarivel', value: selectedExperience.included_lift_ticket },
+  ];
+  const includedList = included.filter(feature => feature.value);
+  const notIncludedList = included.filter(feature => !feature.value);
+
 
   return (
     <>
@@ -64,7 +78,7 @@ const ExperienceView = ({ experienceCard }) => {
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 mb-4">¿Qué incluye la experiencia?</h2>
           <p className="text-sm italic py-5 leading-6 text-gray-900">{selectedExperience.experience_included_description}</p>
           <ul role="list" className="mt-8 grid grid-cols-1 sm:grid-cols-2 gap-x-20 gap-y-5 text-sm leading-6 text-yellow-700">
-            {includedList.map((feature, index) => (
+            {includedList.map((included, index) => (
               <li key={index} className="flex gap-x-4">
                 <CheckIcon className="h-6 w-5 flex-none text-yellow-500 font-extrabold" aria-hidden="true" />
                 {feature.name}
