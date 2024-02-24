f2a32d4970e2b90a2683bbaf3cdad7818caae03b
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-24 18:02:28 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -1,17 +1,14 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-// Importar los íconos y demás componentes necesarios como antes...
-
 const Carousel = ({ experienceCard }) => {
-  const { id } = useParams(); // Obtiene el ID de la URL
+  const { id } = useParams();
   const selectedExperience = experienceCard.find(e => e.experience_uuid === id);
 
   if (!selectedExperience) {
-    return <div></div>;
+    return <div className="text-center">No se encontró la experiencia</div>;
   }
 
-  // Extraer las URLs de las imágenes desde el registro de Supabase
   const imageUrls = [];
   for (let i = 1; i <= 10; i++) {
     const imgUrl = selectedExperience[`card_img_${i}`];
@@ -20,7 +17,6 @@ const Carousel = ({ experienceCard }) => {
     }
   }
 
-  // Componente de Carrusel
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const imagesToShow = 3; // Número de imágenes a mostrar a la vez
 
@@ -38,20 +34,22 @@ const Carousel = ({ experienceCard }) => {
   return (
     <>
       <h2 className="text-3xl text-center font-bold tracking-tight text-gray-900 mb-4">{selectedExperience.experience_name}</h2>
-      <div className="carousel-container">
+      <div className="carousel-container flex justify-center items-center flex-col">
         {imageUrls.length > 0 && (
-          <div>
-            {displayedImages.map((url, index) => (
-              <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} style={{ width: '33%', display: 'inline-block' }} />
-            ))}
-            <div>
-              <button onClick={prevImage}>Anterior</button>
-              <button onClick={nextImage}>Siguiente</button>
+          <div className="flex justify-center items-center">
+            <div className="flex overflow-hidden">
+              {displayedImages.map((url, index) => (
+                <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className="w-full h-48 object-cover" />
+              ))}
+            </div>
+            <div className="flex justify-between w-full">
+              <button onClick={prevImage} className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300">Anterior</button>
+              <button onClick={nextImage} className="px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-700 transition duration-300">Siguiente</button>
             </div>
           </div>
         )}
       </div>
-      {/* Resto del componente ExperienceView... */}
+      {/* Resto del componente Carousel... */}
     </>
   );
 };
