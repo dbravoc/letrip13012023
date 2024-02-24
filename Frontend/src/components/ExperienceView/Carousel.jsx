f1a640eac92dab5c069e2c4abc2f703a1c316f34
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-24 18:53:51 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -39,7 +39,7 @@ const Carousel = ({ experienceCard }) => {
       <div className="carousel-container flex justify-center items-center flex-col">
         {imageUrls.length > 0 && (
           <div className="flex flex-col justify-center items-center">
-            <div className="flex overflow-hidden gap-x-5">
+            <div className="flex justify-center overflow-hidden gap-x-5">
             <button onClick={prevImage} className="px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronLeft} /> </button>
               {displayedImages.map((url, index) => (
                 <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className="w-1/4 h-full object-cover rounded-2xl" />
