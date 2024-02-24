45a4eb599dbe165d2d165c7f84c8e07da2b4668d
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-24 17:53:27 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -8,7 +8,7 @@ const Carousel = ({ experienceCard }) => {
   const selectedExperience = experienceCard.find(e => e.experience_uuid === id);
 
   if (!selectedExperience) {
-    return <div>No se encontró la experiencia</div>;
+    return <div></div>;
   }
 
   // Extraer las URLs de las imágenes desde el registro de Supabase
@@ -22,23 +22,36 @@ const Carousel = ({ experienceCard }) => {
 
   // Componente de Carrusel
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-  const nextImage = () => setCurrentImageIndex((currentImageIndex + 1) % imageUrls.length);
-  const prevImage = () => setCurrentImageIndex((currentImageIndex - 1 + imageUrls.length) % imageUrls.length);
+  const imagesToShow = 3; // Número de imágenes a mostrar a la vez
 
-  // Componentes de la vista y el carrusel integrados...
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + imagesToShow < imageUrls.length) ? prevIndex + imagesToShow : 0);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - imagesToShow >= 0) ? prevIndex - imagesToShow : (imageUrls.length - (imageUrls.length % imagesToShow || imagesToShow)));
+  };
+
+  // Calcular el rango de imágenes a mostrar
+  const displayedImages = imageUrls.slice(currentImageIndex, currentImageIndex + imagesToShow);
+
   return (
     <>
       <h2 className="text-3xl text-center font-bold tracking-tight text-gray-900 mb-4">{selectedExperience.experience_name}</h2>
       <div className="carousel-container">
         {imageUrls.length > 0 && (
           <div>
-            <img src={imageUrls[currentImageIndex]} alt={`Imagen ${currentImageIndex + 1}`} />
-            <button onClick={prevImage}>Anterior</button>
-            <button onClick={nextImage}>Siguiente</button>
+            {displayedImages.map((url, index) => (
+              <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} style={{ width: '33%', display: 'inline-block' }} />
+            ))}
+            <div>
+              <button onClick={prevImage}>Anterior</button>
+              <button onClick={nextImage}>Siguiente</button>
+            </div>
           </div>
         )}
       </div>
-      {/* Resto del componente Carousel... */}
+      {/* Resto del componente ExperienceView... */}
     </>
   );
 };
