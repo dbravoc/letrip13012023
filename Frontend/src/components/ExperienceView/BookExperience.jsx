c42f4ffbce918b9a22fe50440da0070754f63a6e
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-28 14:16:16 -0300

    Update BookExperience.jsx

--- Frontend/src/components/ExperienceView/BookExperience.jsx
+++ Frontend/src/components/ExperienceView/BookExperience.jsx
@@ -16,6 +16,7 @@ const BookExperience = ({ experienceCard }) => {
     customer_email: '',
     customer_address: '',
     approved_terms_and_conditions: false,
+    experience_package: '', // Añadido para manejar la fecha de la experiencia
   });
 
   useEffect(() => {
@@ -34,13 +35,14 @@ const BookExperience = ({ experienceCard }) => {
 
   const handlePlayerChange = (e) => {
     const numPlayers = parseInt(e.target.value, 10);
-    setPlayers(numPlayers);
+    if (!isNaN(numPlayers)) {
+      setPlayers(numPlayers);
+    }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     
-    // Actualizado para que coincida con el servidor desplegado
     const apiUrl = 'https://letrip13012023-backend-lawitec.vercel.app/sold_experiences';
 
     try {
@@ -52,7 +54,7 @@ const BookExperience = ({ experienceCard }) => {
         body: JSON.stringify({
           ...formData,
           experience_uuid: id,
-          customer_phone: parseFloat(formData.customer_phone),
+          number_of_players: players,
         }),
       });
 
@@ -64,7 +66,6 @@ const BookExperience = ({ experienceCard }) => {
       alert('Serás redirigido a la plataforma de pago. Activa la ventana emergente. ¡Nos pondremos en contacto contigo!');
       console.log('Datos guardados:', data);
 
-      // Abre la nueva pestaña solo después de un éxito
       window.open('https://cobros.global66.com/DAVBRA654', '_blank');
     } catch (error) {
       alert('Error al guardar los datos: ' + error.message);
@@ -73,145 +74,67 @@ const BookExperience = ({ experienceCard }) => {
 
   return (
     <div className="mx-0 sm:px-6 mb-10 tracking-tight text-gray-900">
-      {selectedExperience && (
-        <>
-          <div className='pt-10'>
-          <h3 className="text-2xl font-bold  mb-6">Reservar experiencia</h3>
-            <ul className='text-sm text-gray-500 py-4'>
-              <li>1. Ingresa tus datos de contacto.</li>
-              <li>2. Copia el valor total y haz clic en el botón para confirmar tu compra. </li>
-              <li>3. Procede con el pago utilizando nuestras opciones seguras e ingresa el valor total de la experiencia.</li>
-              <li>4. Te contactaremos para confirmar la compra y entregarte toda la información de la experiencia.</li>
-            </ul>
+      {selectedExperience ? (
+        <div className='pt-10'>
+          <h3 className="text-2xl font-bold mb-6">Reservar experiencia</h3>
+          <form onSubmit={handleSubmit}>
+            <label className='text-gray-700 text-sm' htmlFor="experience_package">Elige la fecha de tu experiencia</label>
+            <input
+              id="experience_package"
+              name="experience_package"
+              type="date" // Cambiado a type="date" para seleccionar fecha
+              value={formData.experience_package}
+              onChange={handleChange}
+              className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
+            />
 
-          <form className='gap-y-2 mb-10' onSubmit={handleSubmit}>
-{/* Nombre del Cliente */}
-<label className='text-gray-700 text-sm' htmlFor="customer_name" >Nombres y Apellidos:</label>
-<input
-  id="customer_name"
-  name="customer_name"
-  type="text"
-  value={formData.customer_name}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-{/* Identificación del Cliente */}
-<label className='text-gray-700 text-sm' htmlFor="customer_identification" >Identificación:</label>
-<input
-  id="customer_identification"
-  name="customer_identification"
-  type="text"
-  value={formData.customer_identification}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-{/* Teléfono del Cliente */}
-<label className='text-gray-700 text-sm' htmlFor="customer_phone" >Teléfono:</label>
-<input
-  id="customer_phone"
-  name="customer_phone"
-  type="tel"
-  value={formData.customer_phone}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-{/* Correo Electrónico del Cliente */}
-<label className='text-gray-700 text-sm' htmlFor="customer_email" >Correo Electrónico:</label>
-<input
-  id="customer_email"
-  name="customer_email"
-  type="email"
-  value={formData.customer_email}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-{/* Dirección del Cliente */}
-<label className='text-gray-700 text-sm' htmlFor="customer_address" >Dirección completa, incluyendo Ciudad y País:</label>
-<input
-  id="customer_address"
-  name="customer_address"
-  type="text"
-  value={formData.customer_address}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-
-
-
-
-<label className='text-gray-700 text-sm' htmlFor="experience_package" >Elige la fecha de tu experiencia</label>
-<input
-  id="experience_package"
-  name="experience_package"
-  type="text"
-  value={formData.customer_address}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-<label className='text-gray-700 text-sm' htmlFor="experience_package" >Número de personas</label>
-<input
-  id="players"
-  name="players"
-  type="number"
-  min={experienceCard.minimum_group_size}
-  max={experienceCard.max_group_size}
-  value={formData.players}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
-
-<label className='text-gray-700 text-sm' htmlFor="experience_package" >Valor total</label>
-<input
-  id="players"
-  name="players"
-  type="number"
-  min={experienceCard.minimum_group_size}
-  max={experienceCard.max_group_size}
-  value={formData.players}
-  onChange={handleChange}
-  className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
-/>
+            {/* Corrección en el campo del número de personas */}
+            <label className='text-gray-700 text-sm' htmlFor="players">Número de personas</label>
+            <input
+              id="players"
+              name="players"
+              type="number"
+              min={selectedExperience.minimum_group_size} // Asegurar que se usa selectedExperience
+              max={selectedExperience.max_group_size} // Asegurar que se usa selectedExperience
+              value={players} // Corregido para usar el estado `players`
+              onChange={handlePlayerChange} // Usando handlePlayerChange para manejar este input específicamente
+              className="text-sm block w-full mt-1 p-2 rounded-md border border-gray-300 shadow-sm focus:ring-yellow-700 focus:border-yellow-700 focus:outline-none"
+            />
 
 <div className='pt-10'>
-          <h3 className="text-2xl font-bold  mb-10">Valor total de la experiencia</h3>
-            <p className="block w-1/2 font-semibold text-sm outline-none border-b-2 border-r-2 border-b-letrip border-r-letrip py-2">
+              <h3 className="text-2xl font-bold mb-10">Valor total de la experiencia</h3>
+              <p className="block w-1/2 font-semibold text-sm outline-none border-b-2 border-r-2 border-b-letrip border-r-letrip py-2">
                 <span className="font-semibold text-xl">
-                <FontAwesomeIcon className='text-green-700' icon={faMoneyBills} />
-                {totalPrice.toLocaleString('de-DE')}
-                    </span> USD en total
-            </p>
-</div>
+                  <FontAwesomeIcon className='text-green-700' icon={faMoneyBills} />
+                  {totalPrice.toLocaleString('de-DE')}
+                </span> USD en total
+              </p>
+            </div>
 
-{/* Aceptación de Términos y Condiciones */}
-<div className="mt-4 flex ">
-  <input
-    id="approved_terms_and_conditions"
-    name="approved_terms_and_conditions"
-    type="checkbox"
-    checked={formData.approved_terms_and_conditions}
-    onChange={handleChange}
-  />
-  <label htmlFor="approved_terms_and_conditions" className="pl-2 text-sm text-gray-700 mb-10">
-    Acepto los <a className='font-semibold'>términos y condiciones</a>
-  </label>
-</div>
-          
+            {/* Aceptación de Términos y Condiciones */}
+            <div className="mt-4 flex">
+              <input
+                id="approved_terms_and_conditions"
+                name="approved_terms_and_conditions"
+                type="checkbox"
+                checked={formData.approved_terms_and_conditions}
+                onChange={handleChange}
+              />
+              <label htmlFor="approved_terms_and_conditions" className="pl-2 text-sm text-gray-700">
+                Acepto los <a className='font-semibold'>términos y condiciones</a>
+              </label>
+            </div>
 
-<button type="submit"  className="text-lg hover:bg-black hover:text-letrip bg-letrip text-black py-4 rounded-md text-center w-full block">
-<span className="font-semibold text-2xl">
- Reservar experiencia </span>
-</button>
-</form>
-          </div>
-        </>
+            <button type="submit" className="text-lg hover:bg-black hover:text-letrip bg-letrip text-black py-4 rounded-md text-center w-full block">
+              <span className="font-semibold text-2xl">
+                Reservar experiencia
+              </span>
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div>Precio no disponible</div>
       )}
-      {!selectedExperience && <div>Precio no disponible</div>}
     </div>
   );
 };
