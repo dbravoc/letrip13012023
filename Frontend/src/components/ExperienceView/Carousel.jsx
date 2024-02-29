b2921307a75a62b0a84fdc020da3b1cfc3372f4d
David Bravo Cisterna <bravocisterna@gmail.com>
2024-02-29 16:40:04 -0300

    update

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -35,7 +35,7 @@ const Carousel = ({ experienceCard }) => {
 
   return (
     <>
-    <h2 className="bg-letrip px-4 py-2 text-3xl text-left sm:px-32 font-bold tracking-tight text-gray-900 mb-10">{selectedExperience.experience_name}</h2>
+    <h2 className="bg-letrip mx-4 px-2 py-2 text-3xl text-left sm:px-32 font-bold tracking-tight text-gray-900 mb-10">{selectedExperience.experience_name}</h2>
 
       <div className="carousel-container flex justify-center items-center flex-col">
         {imageUrls.length > 0 && (
