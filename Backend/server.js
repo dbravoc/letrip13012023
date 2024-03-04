e65b732b164b29df7b76135a0ed22316334b1c4f
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-04 12:39:27 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -405,7 +405,7 @@ app.post('/send-email', async (req, res) => {
     const mg = mailgun.client({username: 'api', key: process.env.MAILGUN_KEY || 'key-yourkeyhere'});
   
     mg.messages.create('letriplab.com', {
-      from: "david@letriplab.com",
+      from: "postman@letriplab.com",
       to: [to], // Asegúrate de que 'to' sea un array
       subject,
       text,
