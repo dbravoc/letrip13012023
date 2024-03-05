a8dc0b7ba1a55ec589687d45184016f223cec2e6
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-05 19:27:45 -0300

    Update Info.jsx

--- Frontend/src/components/ExperienceView/Info.jsx
+++ Frontend/src/components/ExperienceView/Info.jsx
@@ -82,7 +82,7 @@ const Info = ({ experienceCard }) => {
 
 </div>
 
-  <div className='grid grid-cols-1 sm:grid-cols-5 gap-8'>
+  <div className='grid grid-cols-1 sm:grid-cols-5 gap-4'>
 
 
     <div className="flex col-span-3 flex-col p-10  bg-white border-white border-2 rounded-2xl">
@@ -105,7 +105,7 @@ const Info = ({ experienceCard }) => {
   </div>
 
 
-    <div className='grid grid-cols-1 sm:grid-cols-5 gap-8'>
+    <div className='grid grid-cols-1 sm:grid-cols-5 gap-4'>
     <div className="flex col-span-3 flex-col p-10  bg-white border-white border-2 rounded-2xl">
           <h2 className="bg-letrip rounded-xl px-4 py-2 text-xl text-left font-bold tracking-tight text-gray-900 mb-4">Itinerario</h2>
           <div className="text-sm italic py-5 leading-6 text-gray-900">{selectedExperience.itinerary}</div>
@@ -139,7 +139,7 @@ const Info = ({ experienceCard }) => {
     </div>
   </div>
   </div>
-  <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
+  <div className='grid grid-cols-1 sm:grid-cols-2 gap-4'>
 
   <div className="flex flex-col p-10  bg-white border-white border-2 rounded-2xl">
           <h2 className="bg-letrip rounded-xl px-4 py-2 text-xl text-left font-bold tracking-tight text-gray-900 mb-4">Alojamiento</h2>
