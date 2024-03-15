353454674b5755f1701d7232e8a11a31a9e931e0
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-15 11:52:54 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -43,9 +43,9 @@ const Carousel = ({ experienceCard }) => {
             <div className="flex justify-center overflow-hidden md:gap-x-5">
             <button onClick={prevImage} className="md:px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronLeft} /> </button>
               {displayedImages.map((url, index) => (
-                <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className={`${window.innerWidth < 640 && index > 0 ? 'hidden' : 'block'} md:w-1/4 w-9/10 h-full object-cover rounded-2xl`} />
+                <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className={`${window.innerWidth < 640 && index > 0 ? 'hidden' : 'block'} md:w-1/4 max-h-max object-cover rounded-2xl`} />
               ))}
-            <button onClick={nextImage} className="px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronRight} /></button>
+            <button onClick={nextImage} className="px-4 py-2 z-10 text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronRight} /></button>
             </div>
           </div>
         )}
