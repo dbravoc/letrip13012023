f55532f461cccba81a8798e90b118a5058d64233
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-15 11:26:18 -0300

    update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -100,7 +100,7 @@ const App = () => {
             <div className="mx-auto w-full">
               <div className="flex flex-col justify-between flex-wrap px-8"><Inicio /></div>
               <div className="flex-wrap pt-44 relative z-10 px-8"><Carousel experienceCard={experienceCard} /></div>
-              <div className="pt-24 md:px-32  px-8"><Info experienceCard={experienceCard} /></div>
+              <div className="pt-24 md:px-32 px-8"><Info experienceCard={experienceCard} /></div>
               <div className="pt-24"><BookExperience experienceCard={experienceCard} /></div>
 
 
