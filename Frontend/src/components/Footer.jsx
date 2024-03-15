51f8d2d6fc5e40d9be18374ad553391133bec304
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-15 16:16:30 -0300

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
+import logo from '/public/img/letrip logo.png';
+
 
 export default function Footer() {
     return (
-      <div className="py-5 border-t-letrip border-t-2 bg-black mt-32 pb-32 pt-12">
+      <>
+      <div className="py-5 border-t-letrip border-t-2 bg-black mt-32 pb-24 pt-12">
           <h2 className="text-center text-lg font-semibold leading-8 text-yellow-100 pb-5">
           ¿Quieres formar parte de Le trip?
           </h2>
@@ -15,6 +18,15 @@ export default function Footer() {
           </Link>
           </div>
       </div>
+      <div className='bg-black flex flex-col justify-center items-center gap-y-2' >
+        <img className="flex h-10 w-auto" src={logo} alt="Logo de LeTrip"/>
+        <p className=' text-yellow-100 text-sm text-center font-bold'>Somos Le Trip SpA, una Travel Tech de experiencias deportivas.</p>
+        <p className=' text-yellow-100 text-sm text-center'>CEO: matias@letriplab.com - CCO: thomas@letriplab.com - CTO: david@letriplab.com</p>
+
+      </div>
+
+      </>
+
     );
   }
   
