170f13a7886a6c2f067611755d58f7e3fbb86da2
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-15 12:17:01 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -42,7 +42,9 @@ const Carousel = ({ experienceCard }) => {
               <FontAwesomeIcon icon={faChevronLeft} /> 
             </button>
             {displayedImages.map((url, index) => (
-              <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className=" md:w-96 block w-full h-full object-cover rounded-2xl" />
+              <div key={index} className="relative md:w-1/3 w-full pb-full rounded-2xl overflow-hidden"> {/* pb-full establece un padding-bottom del 100%, lo que hace el div cuadrado */}
+                <img src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className="absolute inset-0 w-full h-full object-cover" />
+              </div>
             ))}
             <button onClick={nextImage} className="absolute z-30 right-0 px-2 py-2 text-letrip text-4xl font-bold"> 
               <FontAwesomeIcon icon={faChevronRight} />
