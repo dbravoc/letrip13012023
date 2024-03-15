b81ede4bb74dfad6f377de5601896624c1f0afbc
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-15 11:44:17 -0300

    Update Carousel.jsx

--- Frontend/src/components/ExperienceView/Carousel.jsx
+++ Frontend/src/components/ExperienceView/Carousel.jsx
@@ -35,15 +35,15 @@ const Carousel = ({ experienceCard }) => {
 
   return (
     <>
-    <h2 className="sm:mx-36 py-2 text-3xl text-left font-bold tracking-tight text-gray-900 mb-10">{selectedExperience.experience_name}</h2>
+    <h2 className="md:mx-36 py-2 text-3xl text-left font-bold tracking-tight text-gray-900 mb-10">{selectedExperience.experience_name}</h2>
 
       <div className="carousel-container flex justify-center items-center flex-col">
         {imageUrls.length > 0 && (
           <div className="flex flex-col justify-center items-center">
-            <div className="flex justify-center overflow-hidden gap-x-5">
-            <button onClick={prevImage} className="px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronLeft} /> </button>
+            <div className="flex justify-center overflow-hidden md:gap-x-5">
+            <button onClick={prevImage} className="md:px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronLeft} /> </button>
               {displayedImages.map((url, index) => (
-                <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className={`${window.innerWidth < 640 && index > 0 ? 'hidden' : 'block'} w-1/4 h-full object-cover rounded-2xl`} />
+                <img key={index} src={url} alt={`Imagen ${currentImageIndex + index + 1}`} className={`${window.innerWidth < 640 && index > 0 ? 'hidden' : 'block'} md:w-1/4 w-full h-auto object-cover rounded-2xl`} />
               ))}
             <button onClick={nextImage} className="px-4 py-2  text-black font-bold transition duration-300"> <FontAwesomeIcon icon={faChevronRight} /></button>
             </div>
