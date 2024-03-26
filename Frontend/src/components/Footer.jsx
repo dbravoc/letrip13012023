ee2d74773b767496a37dafce93b5f9a8d57d7b90
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-26 18:22:35 -0300

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -21,7 +21,7 @@ export default function Footer() {
       <div className='bg-black flex flex-col justify-center items-center gap-y-2' >
         <img className="flex h-10 w-auto" src={logo} alt="Logo de LeTrip"/>
         <p className=' text-yellow-300 text-sm text-center font-light'>Somos <span className='font-bold'>Le Trip SpA</span>, una <span className='font-bold'>TravelTech</span> de experiencias deportivas.</p>
-        <p className=' text-yellow-100 text-sm text-center font-light'>CEO: matias@letriplab.com - CCO: thomas@letriplab.com - CTO: david@letriplab.com</p>
+        <p className=' text-yellow-100 text-sm text-center font-light'>CEO: matias@letriplab.com - COO: thomas@letriplab.com - CTO: david@letriplab.com</p>
 
       </div>
 
