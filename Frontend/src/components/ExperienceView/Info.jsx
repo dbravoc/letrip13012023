f291518eb2dfe4839234ec78982e769ed39172f5
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-26 17:57:35 -0300

    saltos de linea

--- Frontend/src/components/ExperienceView/Info.jsx
+++ Frontend/src/components/ExperienceView/Info.jsx
@@ -102,8 +102,13 @@ const Info = ({ experienceCard }) => {
     <div className='grid grid-cols-1 md:grid-cols-2 md:gap-4'>
     <div className="flex col-span-1 flex-col md:p-10 py-5  bg-white border-white border-2 rounded-2xl">
           <h2 className="bg-black rounded-xl px-4 py-2 text-xl text-left font-bold tracking-tight text-letrip mb-4">Itinerario</h2>
-          <div className="text-sm italic py-5 leading-6 text-black">{selectedExperience.itinerary}</div>
-    </div>
+          <div className="text-sm italic py-5 leading-6 text-black">
+      {selectedExperience.itinerary.split('\n').map((line, index) => (
+        <React.Fragment key={index}>
+          {line}<br />
+        </React.Fragment>
+      ))}
+    </div>    </div>
 
   <div className='col-span-1'>
     <div className="flex flex-col md:p-10 py-5  bg-white border-white border-2 rounded-2xl">
