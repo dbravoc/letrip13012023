9ff285a630bd02f6591c76724625ae8fbf4d5dc0
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-26 17:29:09 -0300

    Update Info.jsx

--- Frontend/src/components/ExperienceView/Info.jsx
+++ Frontend/src/components/ExperienceView/Info.jsx
@@ -143,7 +143,7 @@ const Info = ({ experienceCard }) => {
 
     <div className="flex col-span-1 flex-col md:p-10 py-5 bg-white border-white border-2 rounded-2xl">
           <h2 className="bg-black rounded-xl px-4 py-2 text-xl text-left font-bold tracking-tight text-letrip mb-4">Restricciones</h2>
-          <ul role="list" className="mt-2 grid grid-cols-1 md:grid-cols-1 gap-x-8 gap-y-1 text-sm leading-6 text-black">
+          <ul className="mt-2 grid grid-cols-1 md:grid-cols-1 gap-x-8 gap-y-1 text-sm leading-6 text-black">
             
               <li key={index} className="flex gap-x-3">
                 <CheckIcon className="h-6 w-5 flex-none text-yellow-500 font-extrabold" aria-hidden="true" />
@@ -155,10 +155,10 @@ const Info = ({ experienceCard }) => {
               </li>
               <li key={index} className="flex gap-x-3">
                 <CheckIcon className="h-6 w-5 flex-none text-yellow-500 font-extrabold" aria-hidden="true" />
-                Mínimo de personas: {selectedExperience.minimum_group_size}              
-              </li>
+                Mínimo de personas: {selectedExperience.minimum_group_size}              </li>
 
           </ul>
+          
     </div>
 
     <div className="flex col-span-1 flex-col md:p-10 py-5 bg-white border-white border-2 rounded-2xl">
