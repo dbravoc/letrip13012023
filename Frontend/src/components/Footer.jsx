38cfb400515ce80ef7fb22b6db20b3a387d2c571
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-27 12:44:13 -0300

    Update Footer.jsx

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -11,7 +11,7 @@ export default function Footer() {
           </h2>
           <div className="flex justify-center items-center">
           <Link 
-            to='/request'
+            to='/inboundform'
             className="relative z-10 flex font-semibold  justify-center items-center mt-1 hover:bg-yellow-100 bg-letrip text-black focus:ring-offset-2 rounded-lg px-4 py-2 ease-in-out duration-50 text-center"
             >Publica tu experiencia con nosotros
 
