a4df6d9c66da5bb4b62aeb7e65c95908a40fcfc6
David Bravo Cisterna <bravocisterna@gmail.com>
2024-03-28 13:10:53 -0300

    Update BookExperience.jsx

--- Frontend/src/components/ExperienceView/BookExperience.jsx
+++ Frontend/src/components/ExperienceView/BookExperience.jsx
@@ -196,7 +196,7 @@ const BookExperience = ({ experienceCard }) => {
                   type="text"
                   value={formData.customer_name}
                   onChange={handleChange}
-                  className="mb-10 text-lg font-semibold block w-full p-2 border-b-2 bg-transparent border-yellow-100 text-gray-900 text-center focus:outline-none border-l-0 border-r-0 border-t-0"
+                  className="mb-10 text-lg font-semibold block w-full p-2 border-b-2 bg-transparent border-yellow-200 text-gray-900 text-center focus:outline-none border-l-0 border-r-0 border-t-0"
                 />
                 
 
@@ -208,7 +208,7 @@ const BookExperience = ({ experienceCard }) => {
                   type="text"
                   value={formData.customer_identification}
                   onChange={handleChange}
-                  className="mb-3 text-lg font-semibold block w-full p-2 border-b-2 bg-transparent border-yellow-100 text-gray-900 text-center focus:outline-none border-l-0 border-r-0 border-t-0"
+                  className="mb-3 text-lg font-semibold block w-full p-2 border-b-2 bg-transparent border-yellow-200 text-gray-900 text-center focus:outline-none border-l-0 border-r-0 border-t-0"
                 />
 
                 {/* Teléfono del Cliente */}
